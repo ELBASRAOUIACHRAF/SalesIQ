ab04e920803e3f32f8e60bac4c467208a7a6ff7e
ELBASRAOUIACHRAF <achraf.elbasraoui@gmail.com>
2025-11-26 00:25:44 +0100

    Start in Services

--- backend/src/main/java/com/ensa/achrafkarim/backend/service/ProductService.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.ensa.achrafkarim.backend.service;
 
+import com.ensa.achrafkarim.backend.dto.ProductDto;
+
+import java.util.List;
+
 public interface ProductService {
+
+    ProductDto createProduct(ProductDto productDto);
+    List<ProductDto> listProducts();
+    ProductDto updateProduct(ProductDto productDto);
+    void deleteProduct(ProductDto productDto);
+    ProductDto getProduct(ProductDto productDto);
+
+
 }
