8bb2b6f411f255ba168273e5391d432bed85bbc1
ELBASRAOUIACHRAF <achraf.elbasraoui@gmail.com>
2025-11-26 01:53:10 +0100

    category implimentation of services V1

--- backend/src/main/java/com/ensa/achrafkarim/backend/dto/CategoryDto.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/dto/CategoryDto.java
@@ -1,7 +1,10 @@
 package com.ensa.achrafkarim.backend.dto;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class CategoryDto {
     private Long id;
     private String name;
--- backend/src/main/java/com/ensa/achrafkarim/backend/service/CategoryServiceImpl.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/service/CategoryServiceImpl.java
@@ -5,7 +5,10 @@ import com.ensa.achrafkarim.backend.entities.Category;
 import com.ensa.achrafkarim.backend.mapper.CategoryMapper;
 import com.ensa.achrafkarim.backend.repository.CategoryRepository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CategoryServiceImpl implements CategoryService {
 
@@ -19,28 +22,41 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public CategoryDto createCategory(CategoryDto categoryDto) {
         Category category = categoryMapper.toEntity(categoryDto);
-        
-        return null;
+        Category categorySaved = categoryRepository.save(category);
+        return categoryMapper.toDto(categorySaved);
     }
 
     @Override
     public CategoryDto updateCategory(Long id, CategoryDto categoryDto) {
-        return null;
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) return null;  // une exception si la categorie n'existe pas
+        category.setUpdatedAt(LocalDateTime.now());
+        category.setName(categoryDto.getName());
+        category.setDescription(categoryDto.getDescription());
+        Category categoryUpdated = categoryRepository.save(category);
+        return categoryMapper.toDto(categoryUpdated);
     }
 
     @Override
     public void deleteCategory(Long id) {
-
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) return;  // une exception si la categorie n'existe pas
+        categoryRepository.deleteById(id);
     }
 
     @Override
     public CategoryDto getCategory(Long id) {
-        return null;
+        Category category = categoryRepository.findById(id).get();
+        return categoryMapper.toDto(category);
     }
 
     @Override
     public List<CategoryDto> listCategories() {
-        return List.of();
+        List<Category> categories = categoryRepository.findAll();
+        List<CategoryDto> categoryDtos = categories.stream()
+                .map(cat -> categoryMapper.toDto(cat))
+                .collect(Collectors.toList());
+        return categoryDtos;
     }
 
 
