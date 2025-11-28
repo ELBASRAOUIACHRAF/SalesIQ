0e30d3048a1f6f6d525c2a60b5abd9e43a8bd257
ELBASRAOUIACHRAF <achraf.elbasraoui@gmail.com>
2025-11-28 12:51:52 +0100

    implementing reviews service methods

--- backend/src/main/java/com/ensa/achrafkarim/backend/repository/ReviewsRepository.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/repository/ReviewsRepository.java
@@ -3,6 +3,7 @@ package com.ensa.achrafkarim.backend.repository;
 import com.ensa.achrafkarim.backend.entities.Reviews;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,9 @@ public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
     List<Reviews> findAllByproductId(Long productId);
 
     List<Reviews> findAllByusersId(Long usersId);
+
+    List<Reviews> findByRatingBetween(double minRating, double maxRating);
+
+    @Query("SELECT AVG(rev.rating) FROM Reviews rev WHERE rev.product.id = :productId")
+    Double findAverageRatingByProductId(Long productId);
 }
--- backend/src/main/java/com/ensa/achrafkarim/backend/service/ReviewsServiceImpl.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/service/ReviewsServiceImpl.java
@@ -3,7 +3,9 @@ package com.ensa.achrafkarim.backend.service;
 import com.ensa.achrafkarim.backend.dto.ReviewsDto;
 import com.ensa.achrafkarim.backend.entities.Reviews;
 import com.ensa.achrafkarim.backend.mapper.ReviewsMapper;
+import com.ensa.achrafkarim.backend.repository.ProductRepository;
 import com.ensa.achrafkarim.backend.repository.ReviewsRepository;
+import com.ensa.achrafkarim.backend.repository.UsersRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,8 @@ public class ReviewsServiceImpl implements ReviewsService{
 
     ReviewsRepository  reviewsRepository;
     ReviewsMapper reviewsMapper;
+    ProductRepository productRepository;
+    UsersRepository usersRepository;
 
     @Override
     public List<ReviewsDto> getReviewsByProduct(Long productId) {
@@ -38,17 +42,23 @@ public class ReviewsServiceImpl implements ReviewsService{
 
     @Override
     public ReviewsDto createReview(ReviewsDto reviewsDto, Long productId, Long userId) {
-        return null;
+        Reviews reviews = reviewsMapper.toEntity(reviewsDto);
+        reviews.setUsers(usersRepository.findById(userId).get());         //Use service instead of repository
+        reviews.setProduct(productRepository.findById(productId).get());  //Use service instead of repository
+        Reviews savedReview = reviewsRepository.save(reviews);
+        return reviewsMapper.toDto(savedReview);
     }
 
     @Override
     public ReviewsDto updateReview(Long reviewId, ReviewsDto reviewsDto) {
+
         return null;
     }
 
     @Override
     public void deleteReview(Long reviewId) {
-
+        if (!reviewsRepository.existsById(reviewId)) return ;
+        reviewsRepository.deleteById(reviewId);
     }
 
     @Override
@@ -58,7 +68,10 @@ public class ReviewsServiceImpl implements ReviewsService{
 
     @Override
     public List<ReviewsDto> getReviewsByRatingRange(double minRating, double maxRating) {
-        return List.of();
+        List<Reviews> reviewsList = reviewsRepository.findByRatingBetween(minRating,maxRating);
+        return reviewsList.stream()
+                .map(rev -> reviewsMapper.toDto(rev))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -68,7 +81,8 @@ public class ReviewsServiceImpl implements ReviewsService{
 
     @Override
     public double getAverageRatingByProduct(Long productId) {
-        return 0;
+        double averageRating = reviewsRepository.findAverageRatingByProductId(productId);
+        return averageRating;
     }
 
     @Override
