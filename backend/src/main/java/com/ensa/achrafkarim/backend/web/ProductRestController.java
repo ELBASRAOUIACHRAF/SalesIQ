d28eafc94476e29ee04e7c1ffb602f5293e0c08c
ELBASRAOUIACHRAF <achraf.elbasraoui@gmail.com>
2025-11-29 15:10:48 +0100

    add product rest controller methods

--- backend/src/main/java/com/ensa/achrafkarim/backend/web/ProductRestController.java
+++ backend/src/main/java/com/ensa/achrafkarim/backend/web/ProductRestController.java
@@ -66,4 +66,72 @@ public class ProductRestController {
         return productService.updateProduct(productDto);
     }
 
+    @GetMapping("/lowstockalert/{productId}")
+    public boolean lowstockalert(@PathVariable Long productId){
+        return productService.isLowStock(productId);
+    }
+
+    @GetMapping("/lowStockProducts")
+    public List<ProductDto> lowStockProducts(){
+        return productService.getLowStockProducts();
+    }
+
+    @GetMapping("/availabaleProducts")
+    public List<ProductDto>  availabaleProducts(){
+        return productService.getAvailableProducts();
+    }
+
+    @GetMapping("/bestSellingProducts/{limit}")
+    public List<ProductDto>  bestSellingProducts(int limit){
+        return productService.getBestSellingProducts(limit);
+    }
+
+    @GetMapping("/leastSellingProducts/{limit}")
+    public List<ProductDto>  leastSellingProducts(int limit){
+        return productService.getLeastSellingProducts(limit);
+    }
+
+    @GetMapping("/noSalesProducts")
+    public List<ProductDto>  noSalesProducts(){
+        return productService.getProductsWithNoSales();
+    }
+
+    @GetMapping("/unAvailabaleProducts")
+    public List<ProductDto>  unAvailabaleProducts(){
+        return productService.getUnavailableProducts();
+    }
+
+    @GetMapping("/productsByFilter")
+    public List<ProductDto>  productsByFilter(@RequestParam String name,
+                                              @RequestParam Long categoryId,
+                                              @RequestParam Double minPrice,
+                                              @RequestParam Double maxPrice){
+        return productService.filterProducts(name, categoryId, minPrice, maxPrice);
+    }
+
+    @GetMapping("/topPofitProducts")
+    public List<ProductDto>  topPofitProducts(){
+        return productService.getTopProfitProduct();
+    }
+
+    @PutMapping("/markProductAvailable/{productaId}")
+    public void markProductAvailable(@PathVariable Long productaId){
+        productService.markProductAsAvailable(productaId);
+    }
+
+    @PutMapping("/markProductUnAvailable/{productaId}")
+    public void markProductUnAvailable(@PathVariable Long productaId){
+        productService.markProductAsUnavailable(productaId);
+    }
+
+    @PostMapping("/createProductsBulk")
+    public List<ProductDto>  createProductsBulk(@RequestBody List<ProductDto> products){
+        return productService.createProductsBulk(products);
+    }
+
+    @DeleteMapping("/deleteProductsBulk")
+    public void  deleteProductsBulk(@RequestBody List<Long> products){
+        productService.deleteProductsBulk(products);
+    }
+
 }
